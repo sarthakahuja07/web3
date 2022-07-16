ac464383645bbc5d2d0e926c81798c17109bad19
sarthak <sarthak.ahuja0007@gmail.com>
2022-07-16 21:38:35 +0530

    transfer, call, send , withdraw function

--- solidity/FundMe.sol
+++ solidity/FundMe.sol
@@ -2,10 +2,13 @@
 
 pragma solidity ^0.8.0;
 
-import './libraries/PriceConvertor.sol';
+import "./PriceConvertor.sol";
 
 contract FundMe {
     using PriceConvertor for uint256;
+
+
+
     uint256 minUsd = 1 * 1e18;
     address[] funders;
     mapping(address => uint256) addressToFunds;
@@ -15,6 +18,21 @@ contract FundMe {
         require(msg.value.getConversionRate() > minUsd, "minimum of 10USD needed");
         funders.push(msg.sender);
         addressToFunds[msg.sender] += msg.value;
+    }
+
+    function withdraw() public {
+        for(uint i=0; i<funders.length; i++){
+            address founderAddress=funders[i];
+            addressToFunds[founderAddress]=0;
+        }
+        funders = new address[](0);
+        //  to transfer - 3 methods - send,transfer, call
+        // payable(msg.sender).transfer(address(this).balance); 
+    
+        // payable(msg.sender).send(address(this).balance);
+
+        (bool success, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(success, "withdraw failed");
     }
 
     function getFundsFromAdrress (address funder) public view returns (uint256){
