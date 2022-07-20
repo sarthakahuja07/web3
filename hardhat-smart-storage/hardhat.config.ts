4d557f00a46ec56ffe92a2d9fa84b0c341076e6f
sarthak <sarthak.ahuja0007@gmail.com>
2022-07-20 21:09:28 +0530

    gas reporter

--- hardhat-smart-storage/hardhat.config.ts
+++ hardhat-smart-storage/hardhat.config.ts
@@ -2,7 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "./tasks/block-number";
 import "./tasks/accounts";
-
+import "hardhat-gas-reporter";
 require("dotenv").config();
 
 const config: HardhatUserConfig = {
@@ -14,14 +14,20 @@ const config: HardhatUserConfig = {
 			chainId: 4,
 			accounts: [process.env.ACCOUNT_1!]
 		},
-        localhost:{
-            url:"http://127.0.0.1:8545/",
-            chainId : 31337
-        }
+		localhost: {
+			url: "http://127.0.0.1:8545/",
+			chainId: 31337
+		}
 	},
-    etherscan : {
-        apiKey: process.env.ETHERSCAN_API_KEY,
-    }
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_API_KEY
+	},
+	gasReporter: {
+		enabled: true,
+		currency: "INR",
+		coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+        token:'MATIC'
+	}
 };
 
 export default config;
