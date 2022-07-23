6f293adf02e604b0484babc8deaf8dee6db8f20b
sarthak <sarthak.ahuja0007@gmail.com>
2022-07-24 05:14:00 +0530

    AggregatorV3Interface priceFeed

--- hardhat-fundMe/contracts/PriceConvertor.sol
+++ hardhat-fundMe/contracts/PriceConvertor.sol
@@ -5,21 +5,21 @@ pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 library PriceConvertor {
-     function getPrice() internal view returns (uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
-        (
-            ,
-            int price,
-            ,
-            ,
-        ) = priceFeed.latestRoundData();
+    function getPrice(AggregatorV3Interface priceFeed)
+        internal
+        view
+        returns (uint256)
+    {
+        (, int price, , , ) = priceFeed.latestRoundData();
         return uint256(price * 1e10);
     }
 
-    function getConversionRate(uint256 ethAmount) internal view returns (uint256){
-        uint256 ethPrice = getPrice();
-        uint256 priceInUSD =  (ethPrice *ethAmount ) / 1e18;
+    function getConversionRate(
+        uint256 ethAmount,
+        AggregatorV3Interface priceFeed
+    ) internal view returns (uint256) {
+        uint256 ethPrice = getPrice(priceFeed);
+        uint256 priceInUSD = (ethPrice * ethAmount) / 1e18;
         return priceInUSD;
     }
-
 }
